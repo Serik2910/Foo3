f08458fbd5b997109e157aa41c66f861349c368c
kobla <koblandy@inbox.ru>
2021-08-21 14:11:37 +0600

    clone

---
+++ Program.java
@@ -0,0 +1,12 @@
+public class Program {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome java");
+        Zebra zebra = new Zebra();
+        zebra.say();
+
+        Giraffe giraffe = new Giraffe();
+        giraffe.say(); //another comment
+
+    }
+}
